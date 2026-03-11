eb3bc84f6865ea1d9dd93e214d0eac2b5286ab09
Ariel Sousa Dias <114015507+perigoboy@users.noreply.github.com>
2026-03-11 09:42:18 -0300

    feat: configurar seguranca com helmet e cors T008

--- src/server.js
+++ src/server.js
@@ -1,12 +1,19 @@
 const express = require('express');
+const cors = require('cors'); // Importa o CORS
+const helmet = require('helmet'); // Importa o Helmet
 const db = require('./models');
 const userRoutes = require('./routes/userRoutes'); // Importando sua organização
 const healthRoutes = require('./routes/healthRoutes'); // Importando sua organização
 
 
 const app = express();
+
+// SEGURANÇA E CONFIGURAÇÕES
+app.use(helmet()); // Protege os headers HTTP
+app.use(cors()); // Libera o acesso para o frontend
 app.use(express.json());
 
+// ROTAS
 // Definindo o prefixo das rotas (padrão de API)
 app.use('/api/users', userRoutes);
 app.use('/health', healthRoutes); // Rota direta para monitoramento
@@ -19,7 +26,7 @@ db.sequelize.authenticate()
     console.log('✅ Conexão com o Banco: OK');
     console.log('📂 Estrutura de Pastas: OK');
     app.listen(PORT, () => {
-      console.log(`🚀 Openest API rodando em http://localhost:${PORT}`);
+      console.log(`🚀 Openest API segura rodando em http://localhost:${PORT}`);
     });
   })
   .catch(err => {
