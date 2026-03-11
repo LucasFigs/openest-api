637ce5d1b3b7f02611546533d151e6f130e935ea
Ariel Sousa Dias <114015507+perigoboy@users.noreply.github.com>
2026-03-11 09:52:38 -0300

    feat: configurar rate limiting para proteção contra ataques T012

--- src/server.js
+++ src/server.js
@@ -1,9 +1,11 @@
-const express = require('express');
+const express = require('express'); // Importa o Express
 const cors = require('cors'); // Importa o CORS
 const helmet = require('helmet'); // Importa o Helmet
-const db = require('./models');
+const rateLimit = require('express-rate-limit'); // Importa o rate limiter
+const db = require('./models'); // Importa a configuração do Sequelize
 const userRoutes = require('./routes/userRoutes'); // Importando sua organização
 const healthRoutes = require('./routes/healthRoutes'); // Importando sua organização
+
 
 
 const app = express();
@@ -11,7 +13,15 @@ const app = express();
 // SEGURANÇA E CONFIGURAÇÕES
 app.use(helmet()); // Protege os headers HTTP
 app.use(cors()); // Libera o acesso para o frontend
-app.use(express.json());
+app.use(express.json()); // Permite receber JSON no corpo das requisições
+
+// Aplicando Rate Limit
+const limiter = rateLimit({
+  windowMs: 15 * 60 * 1000,
+  max: 100,
+  message: { message: "Muitas requisições, tente mais tarde." }
+});
+app.use(limiter); 
 
 // ROTAS
 // Definindo o prefixo das rotas (padrão de API)
