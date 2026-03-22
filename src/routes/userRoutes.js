f8c5961bf4e9431d1c04d025445fa64f67eeddeb
Ariel Sousa Dias <114015507+perigoboy@users.noreply.github.com>
2026-03-22 17:28:12 -0300

    feat: configurar Cloudinary e estrutura de upload de imagens T011

--- src/routes/userRoutes.js
+++ src/routes/userRoutes.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const router = express.Router();
 const userController = require('../controllers/userController');
+const uploadCloud = require('../config/cloudinary');
 
 // Rota de teste para confirmar que a estrutura funciona
 router.get('/health', (req, res) => {
@@ -14,5 +15,15 @@ router.get('/health', (req, res) => {
 // Rota para registrar um novo usuário
 router.post('/register', userController.register);
 router.post('/login', userController.login);
+
+// Rota temporária para testar o upload
+router.post('/upload-test', uploadCloud.single('image'), (req, res) => {
+  if (!req.file) {
+    return res.status(400).json({ error: "Nenhuma imagem enviada." });
+  }
+  // O Cloudinary retorna a URL da imagem em req.file.path
+  res.json({ url: req.file.path });
+});
+
 
 module.exports = router;
